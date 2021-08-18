a4429ea4888938bb96dafafb9db2b5db8db383b4
Apnil0910 <apnildavenfs@gmail.com>
2021-08-18 17:56:39 +0530

    test case commit

--- src/test/java/com/app/spring/boot/jenkins/springbootexample/SpringBootExampleApplicationTests.java
+++ src/test/java/com/app/spring/boot/jenkins/springbootexample/SpringBootExampleApplicationTests.java
@@ -1,13 +1,21 @@
 package com.app.spring.boot.jenkins.springbootexample;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class SpringBootExampleApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testGetDetails() {
+		
+		
+		String url = "http://localhost:8081/details" ;
+		RestTemplate rst = new RestTemplate();
+		String str = rst.getForObject(url, String.class);
+		assertNotNull(str);
 	}
 
 }
